f2fa712826091e6926c2f41465167812ae0f7251
Eric <erics.secondary.email@gmail.com>
2018-03-11 18:14:52 -0700

    WIP

--- app/controllers/ethnic_churches_controller.rb
+++ app/controllers/ethnic_churches_controller.rb
@@ -7,4 +7,19 @@ class EthnicChurchesController < ApplicationController
   def show
     @ethnic_church = EthnicChurch.find(params[:id])
   end
+
+  def new
+    @ethnic_church = EthnicChurch.new
+  end
+
+  def create
+    p params
+    ec_params = params[:ethnic_church]
+    lang = params[:language]
+    country = params[:country]
+    religious_background = params[:religious_background]
+    address = params[:address]
+  end
+  
+  # <ActionController::Parameters {"utf8"=>"✓", "ethnic_church"=>{"name"=>"Coptic Church of Footown", "phone"=>"1234567890", "website"=>"https://example.com/", "pastors_name"=>"John Doe", "email"=>"foo@example.com"}, "language"=>{"name"=>"Arabic"}, "country"=>{"name"=>"Egypt"}, "religious_background"=>{"persuasion"=>"Coptic"}, "address"=>{"street"=>"1234 Foostreet", "city"=>"Footown", "zip"=>"99999"}, "note"=>{"content"=>"This is a note."}, "commit"=>"Create", "controller"=>"ethnic_churches", "action"=>"create"} permitted: false>
 end
